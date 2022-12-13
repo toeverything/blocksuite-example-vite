833969b70a32a6bd312e044ddd3033964680edd1
Yifeng Wang <doodlewind@toeverything.info>
2022-12-13 20:26:32 +0800

    feat: add minimal example

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,42 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import { setupCounter } from './counter'
+import "@blocksuite/blocks";
+import "@blocksuite/editor";
+import "@blocksuite/blocks/style";
+import { createEditor, createDebugMenu, BlockSchema } from "@blocksuite/editor";
+import { Workspace, Page } from "@blocksuite/store";
+import "./style.css";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+/**
+ * Manually create initial page structure.
+ * In collaboration mode or on page refresh with local persistence,
+ * the page structure will be automatically loaded from provider.
+ * In these cases, this function should not be called.
+ */
+function createInitialPage(workspace: Workspace) {
+  workspace.signals.pageAdded.once((id) => {
+    const page = workspace.getPage(id) as Page;
+    const pageBlockId = page.addBlock({ flavour: "affine:page" });
+    const groupId = page.addBlock({ flavour: "affine:group" }, pageBlockId);
+    page.addBlock({ flavour: "affine:paragraph" }, groupId);
+  });
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+  workspace.createPage("page0");
+}
+
+// Subscribe for page update and create editor after page loaded.
+function subscribePage(workspace: Workspace) {
+  workspace.signals.pageAdded.once((pageId) => {
+    const page = workspace.getPage(pageId) as Page;
+    const editor = createEditor(page);
+    const debugMenu = createDebugMenu(workspace, editor);
+    document.body.appendChild(editor);
+    document.body.appendChild(debugMenu);
+  });
+}
+
+function main() {
+  const workspace = new Workspace({}).register(BlockSchema);
+  subscribePage(workspace);
+  createInitialPage(workspace);
+}
+
+main();
